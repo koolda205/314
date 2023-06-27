9eda1589d4b13e3f78a39a1160f6f97b4282fbcc
Dmitrii Kulakov <koolda@yandex.ru>
2023-06-27 12:59:41 +0300

    2

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserRepository.java
@@ -4,7 +4,11 @@ package ru.kata.spring.boot_security.demo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kata.spring.boot_security.demo.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername (String username);
 
 
 }
