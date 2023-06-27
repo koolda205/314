98b76c0ce8620457737f53ff2fa1d00a065c218a
Dmitrii Kulakov <koolda@yandex.ru>
2023-06-27 23:15:12 +0300

    27_03 w

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -48,6 +51,19 @@ public class UserServiceImpl implements UserService {
         userDao.deleteUserByID(id);
     }
 
+//    @Transactional
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+//        Optional<User> user = userRepository.findByUsername(username);
+//
+//        if (user.isEmpty()) {
+//            throw new UsernameNotFoundException("User not found");
+//        }
+//        return new UsersDetails(user.get());
+//        return null;
+
+//    }
 }
 
 
