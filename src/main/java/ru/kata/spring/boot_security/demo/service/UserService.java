1265b4aaf99a56dde8f2ca6c83d0ffbb392bde81
Dmitrii Kulakov <koolda@yandex.ru>
2023-06-27 22:57:07 +0300

    27_07m dw

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -2,11 +2,12 @@ package ru.kata.spring.boot_security.demo.service;
 
 
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 
-//public interface UserService extends UserDetailsService {
+@Service
 public interface UserService extends UserDetailsService {
 
     List<User> getAllUsers();
