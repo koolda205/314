5bd8ea5f261a23e0d4f20ce2a01b492a62c886a2
Dmitrii Kulakov <koolda@yandex.ru>
2023-07-05 12:20:10 +0300

    05-07 3

--- src/main/java/ru/kata/spring/boot_security/demo/security/SecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
-                .loginPage("/login")
+                .loginPage("/")
                 .successHandler(loginSuccessHandler)
                 .loginProcessingUrl("/login")
                 .usernameParameter("j_login")
