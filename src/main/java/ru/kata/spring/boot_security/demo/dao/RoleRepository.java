5e4328c74ef38e3c86948fa665cab0d6a3111598
Dmitrii Kulakov <koolda@yandex.ru>
2023-07-27 21:34:16 +0300

    before add UserRepositiry

--- src/main/java/ru/kata/spring/boot_security/demo/dao/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import ru.kata.spring.boot_security.demo.model.Role;
 
 public interface RoleRepository extends CrudRepository <Role, Long> {
+
 }
