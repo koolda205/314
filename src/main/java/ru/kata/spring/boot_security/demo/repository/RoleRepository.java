39d8016e12bbb8436cbf39679bac305d644f8594
Dmitrii Kulakov <koolda@yandex.ru>
2023-08-08 19:44:28 +0300

    final

--- src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
@@ -2,8 +2,6 @@ package ru.kata.spring.boot_security.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kata.spring.boot_security.demo.model.Role;
-import ru.kata.spring.boot_security.demo.model.User;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByUserRole(String userRole);
 }
