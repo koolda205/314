257032ebc4e0105ca82d023db660bea044e209f7
Dmitrii Kulakov <koolda@yandex.ru>
2023-08-17 18:04:58 +0300

    dw

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -11,8 +11,10 @@ public class SpringBootSecurityDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
 	}
+
 	@Bean
 	public ModelMapper modelMapper () {
-	return new ModelMapper();
+		return new ModelMapper();
 	}
+
 }
