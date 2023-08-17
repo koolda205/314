8167f246a15257f11d3d23d47b4b78407acaa148
Dmitrii Kulakov <koolda@yandex.ru>
2023-08-17 23:54:54 +0300

    dw

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,86 +1,279 @@
-    const url = 'http://localhost:8080/api/users/';
-    const container = document.querySelector('tbody');
-    let result = '';
+"use strict";
 
-    //////////////////////////////////////////////
+const url = "http://localhost:8080/api/users/"
 
+async function getIndexPage() {
+    let page = await fetch(url);
+    if (page.ok) {
+        let listAllUser = await page.json();
+        loadTableData(listAllUser);
+    } else {
+        alert(`Error, ${page.status}`)
+    }
+}
 
-    // const show = (users) => {
-    //     users.forEach(user => {
-    //         results +=  <tr>
-    //                         <td>${user.id}</td>
-    //                         <td>${user.name}</td>
-    //                         <td>${user.surname}</td>
-    //                         <td>${user.age}</td>
-    //                         <td>${user.email}</td>
-    //                         <td>${user.roles}</td>
-    //                         <td class="text-center"><a class="btnEdit btn btn-primary">Edit</a></td>
-    //                         <td className="text-center"><a className="btnDelete btn btn-danger">Delete</a></td>
-    //                     </tr>
-    // })
-    // container.innerHTML = results;
-    // }
+const pills = document.querySelectorAll('.pill');
+const pillsContent = document.querySelectorAll('.pillContent');
+pills.forEach((clickedPill) => {
+    clickedPill.addEventListener('click', async () => {
+        pills.forEach((pill) => {
+            pill.classList.remove('active');
+        });
+        clickedPill.classList.add('active');
+        let tabId = clickedPill.getAttribute('id');
+        await activePillContent(tabId);
+    });
+});
 
-///////////////////////////////////////////////////////////////////
-      // fetch(url)
-      //   .then( response => response.json() )
-      //   .then( data => show(data) )
-      //   .catch( error => console.log(error) );
-/////////////////////////////////////////////////////////////////
-// fetch(url)
-//     .then(res => {if(res.ok) {
-//         return res.json();
-//     } else {
-//         console.log("ERROR");
-//         throw Error;
-//     }
-//     })
-//     .then(data =>
-//         document.getElementById('root').innerHTML =
-//         JSON.stringify(data)
-//     ).catch(error => {
-//         console.log(error);
-//     })
+async function activePillContent(tabId) {
+    pillsContent.forEach((clickedPillContent) => {
+        clickedPillContent.classList.contains(tabId) ?
+            clickedPillContent.classList.add('active') :
+            clickedPillContent.classList.remove('active');
+    })
+}
 
-    async function getIndexPage() {
-        let page = await fetch(url);
-        if (page.ok) {
-            let listAllUser = await page.json();
-            loadTableData(listAllUser);
-        } else {
-            alert(`Error, ${page.status}`)
+async function getMyUser() {
+    let res = await fetch('/api/auth');
+    let resUser = await res.json();
+    userNavbarDetails(resUser);
+}
+
+window.addEventListener('DOMContentLoaded', getMyUser);
+
+function userNavbarDetails(resUser) {
+    let userList = document.getElementById('myUserDetails');
+    let roles = ''
+    for (let role of resUser.roles) {
+        roles += role.name + ' '
+    }
+    userList.insertAdjacentHTML('beforeend', `
+        <b> ${resUser.username} </b> with roles: <a>${roles} </a>`);
+}
+
+function loadTableData(listAllUser) {
+    let tableBody = document.getElementById('tbody');
+    let dataHtml = '';
+    for (let user of listAllUser) {
+        let roles = [];
+        for (let role of user.roles) {
+            roles.push(" " + role.name)
+        }
+        dataHtml +=
+            `<tr>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.age}</td>
+    <td>${user.username}</td>
+    <td>${roles}</td>
+    <td>
+        <button class="btn blue-background" data-bs-toogle="modal"
+        data-bs-target="#editModal"
+        onclick="editModalData(${user.id})">Edit</button>
+    </td>
+        <td>
+        <button class="btn btn-danger" data-bs-toogle="modal"
+        data-bs-target="#deleteModal"
+        onclick="deleteModalData(${user.id})">Delete</button>
+    </td>
+</tr>`
+    }
+    tableBody.innerHTML = dataHtml;
+}
+
+getIndexPage();
+
+window.addEventListener('DOMContentLoaded', loadUserTable);
+
+async function loadUserTable() {
+    let tableBody = document.getElementById('tableUser');
+    let page = await fetch("/api/users");
+    let currentUser;
+    if (page.ok) {
+        currentUser = await page.json();
+    } else {
+        alert(`Error, ${page.status}`)
+    }
+    let dataHtml = '';
+    let roles = [];
+    for (let role of currentUser.roles) {
+        roles.push(" " + role.name)
+    }
+    dataHtml +=
+        `<tr>
+    <td>${currentUser.id}</td>
+    <td>${currentUser.firstName}</td>
+    <td>${currentUser.lastName}</td>
+    <td>${currentUser.age}</td>
+    <td>${currentUser.username}</td>
+    <td>${roles}</td>
+</tr>`
+    tableBody.innerHTML = dataHtml;
+}
+
+const tabs = document.querySelectorAll('.taba');
+const tabsContent = document.querySelectorAll('.tabaContent');
+tabs.forEach((clickedTab) => {
+    clickedTab.addEventListener('click', async () => {
+        tabs.forEach((tab) => {
+            tab.classList.remove('active');
+        });
+        clickedTab.classList.add('active');
+        let tabaId = clickedTab.getAttribute('id');
+        await activeTabContent(tabaId);
+    });
+});
+
+async function activeTabContent(tabaId) {
+    tabsContent.forEach((clickedTabContent) => {
+        clickedTabContent.classList.contains(tabaId) ?
+            clickedTabContent.classList.add('active') :
+            clickedTabContent.classList.remove('active');
+    })
+}
+
+const form_new = document.getElementById('new');
+
+async function newUser() {
+    form_new.addEventListener('submit', addNewUser);
+}
+
+async function addNewUser(event) {
+    event.preventDefault();
+    let listOfRole = [];
+    for (let i = 0; i < form_new.roleSelect.options.length; i++) {
+        if (form_new.roleSelect.options[i].selected) {
+            listOfRole.push({id: form_new.roleSelect.options[i].value,
+                role: form_new.roleSelect.options[i].text});
         }
     }
-
-    function loadTableData(listAllUser) {
-        let tableBody = document.getElementById('tbody');
-        let dataHtml = '';
-        for (let user of listAllUser) {
-            let roles = [];
-            for (let role of user.roles) {
-                roles.push(" " + role.name)
-            }
-            dataHtml +=
-                `<tr>
-                    <td>${user.id}</td>
-                    <td>${user.name}</td>
-                    <td>${user.surname}</td>
-                    <td>${user.age}</td>
-                    <td>${user.username}</td>
-                    <td>${roles}</td>
-                    <td>
-                        <button class="btn blue-background" data-bs-toogle="modal"
-                           data-bs-target="#editModal"
-                           onclick="editModalData(${user.id})">Edit</button>
-                    </td>
-                    <td>
-                        <button class="btn btn-danger" data-bs-toogle="modal"
-                        data-bs-target="#deleteModal"
-                        onclick="deleteModalData(${user.id})">Delete</button>
-                    </td>
-                </tr>`
-        }
-        tableBody.innerHTML = dataHtml;
+    let method = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            firstName: form_new.firstName.value,
+            lastName: form_new.lastName.value,
+            age: form_new.age.value,
+            username: form_new.username.value,
+            password: form_new.password.value,
+            roles: listOfRole
+        })
     }
+    await fetch(url, method).then(() => {
+        form_new.reset();
+        getIndexPage();
+        activeTabContent('home-tab');
+        let activateTab = document.getElementById('home-tab');
+        activateTab.classList.add('active');
+        let deactivateTab = document.getElementById('profile-tab');
+        deactivateTab.classList.remove('active');
+    });
+}
 
-    getIndexPage();
+const form_ed = document.getElementById('formForEditing');
+const id_ed = document.getElementById('id_ed');
+const firstName_ed = document.getElementById('firstName_ed');
+const lastName_ed = document.getElementById('lastName_ed');
+const age_ed = document.getElementById('age_ed');
+const username_ed = document.getElementById('username_ed');
+const password_ed = document.getElementById('password_ed');
+
+
+async function editModalData(id) {
+    $('#editModal').modal('show');
+    const urlDataEd = 'http://localhost:8080/admin/users/' + id;
+    let usersPageEd = await fetch(urlDataEd);
+    if (usersPageEd.ok) {
+        await usersPageEd.json().then(user => {
+            id_ed.value = `${user.id}`;
+            firstName_ed.value = `${user.firstName}`;
+            lastName_ed.value = `${user.lastName}`;
+            age_ed.value = `${user.age}`;
+            username_ed.value = `${user.username}`;
+            password_ed.value = `${user.password}`;
+        })
+    } else {
+        alert(`Error, ${usersPageEd.status}`)
+    }
+}
+
+async function editUser() {
+    let urlEdit = 'http://localhost:8080/admin/users/' + id_ed.value;
+    let listOfRole = [];
+    for (let i = 0; i < form_ed.rolesForEditing.options.length; i++) {
+        if (form_ed.rolesForEditing.options[i].selected) {
+            listOfRole.push({id: form_ed.rolesForEditing.options[i].value,
+                name: form_ed.rolesForEditing.options[i].text});
+        }
+    }
+    let method = {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: form_ed.editedUserId.value,
+            firstName: form_ed.firstName.value,
+            lastName: form_ed.lastName.value,
+            age: form_ed.age.value,
+            username: form_ed.username.value,
+            password: form_ed.password.value,
+            roles: listOfRole
+        })
+    }
+    await fetch(urlEdit, method).then(() => {
+        $('#editCloseBtn').click();
+        getIndexPage();
+    })
+}
+
+const form_del = document.getElementById('formForDeleting');
+const id_del = document.getElementById('id_del');
+const firstName_del = document.getElementById(`firstName_del`);
+const lastName_del = document.getElementById('lastName_del');
+const age_del = document.getElementById('age_del');
+const username_del = document.getElementById('username_del');
+const password_del = document.getElementById('password_del');
+
+
+async function deleteModalData(id) {
+    $('#deleteModal').modal('show');
+    const urlForDel = 'http://localhost:8080/admin/users/' + id;
+    let usersPageDel = await fetch(urlForDel);
+    if (usersPageDel.ok) {
+        await usersPageDel.json().then(user => {
+            id_del.value = `${user.id}`;
+            firstName_del.value = `${user.name}`;
+            lastName_del.value = `${user.surname}`;
+            age_del.value = `${user.age}`;
+            username_del.value = `${user.email}`;
+            password_del.value = `${user.password}`;
+        })
+    } else {
+        alert(`Error, ${usersPageDel.status}`)
+    }
+}
+
+async function deleteUser() {
+    let urlDel = 'http://localhost:8080/admin/users/' + id_del.value;
+    let method = {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            firstName: form_del.firstName.value,
+            lastName: form_del.lastName.value,
+            age: form_del.age.value,
+            username: form_del.username.value,
+            password: form_del.password.value
+        })
+    }
+    await fetch(urlDel, method).then(() => {
+        $('#deleteCloseBtn').click();
+        getIndexPage();
+    })
+}
